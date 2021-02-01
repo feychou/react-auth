a9293f3ba14e5e60d55ac543ac9a883054bab24b
feychou <federica.recanatini@gmail.com>
2021-02-01 13:55:13 +0100

    isolate nav component

--- src/App.js
+++ src/App.js
@@ -1,48 +1,22 @@
-import React, { useContext } from "react";
+import React from "react";
 import Home from "./pages/Home"
 import Admin from "./pages/Admin"
 import Login from "./pages/Login"
 import Signup from "./pages/Signup"
-import { Link, Route, Switch } from "react-router-dom";
-import { AuthContext } from "./AuthContext"
+import { Route, Switch } from "react-router-dom";
 import ProtectedRoute from './components/ProtectedRoute'
+import Nav from './components/Nav';
 
-const App = () => {
-  const { isLoggedIn, logout } = useContext(AuthContext);
-
-  return (
-    <div className="App">
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/admin">Admin</Link>
-        </li>
-        <li>
-          {
-            isLoggedIn() ?
-            <Link onClick={logout}>Logout</Link> :
-            <Link to="/login">Login</Link>
-          }
-        </li>
-        {
-          !isLoggedIn() &&
-          <li>
-            <Link to="/signup">Sign up</Link>
-          </li>
-        }
-      </ul>
-      <Switch>
-        <ProtectedRoute path="/admin" component={Admin} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/signup" component={Signup} />
-        <Route exact path="/" component={Home} />
-      </Switch>
-      
-    </div>
-  );
-};
-
+const App = () => (
+  <div className="App">
+    <Nav />
+    <Switch>
+      <ProtectedRoute path="/admin" component={Admin} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/signup" component={Signup} />
+      <Route exact path="/" component={Home} />
+    </Switch>
+  </div>
+);
 
 export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,30 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../AuthContext"
+
+const Nav = () => {
+  const { isLoggedIn, logout } = useContext(AuthContext);
+
+  return (
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/admin">Admin</Link></li>
+      <li>
+        {
+          isLoggedIn() ?
+          <Link onClick={logout}>Logout</Link> :
+          <Link to="/login">Login</Link>
+        }
+      </li>
+      {
+        !isLoggedIn() &&
+        <li>
+          <Link to="/signup">Sign up</Link>
+        </li>
+      }
+    </ul>      
+  );
+};
+
+
+export default Nav;
